1e735b5517b925e71366cdc3d31826eac3a1dad7
rob <rjgallac@hotmail.com>
2019-09-28 15:33:22 +0100

    test camera input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'planttracker2';
   constructor(){
-    document.body.requestFullscreen();
+    // document.body.requestFullscreen();
   }
 }
